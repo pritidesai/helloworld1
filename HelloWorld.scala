dd4048c068c1e394b2be5c614616aed179a76e68
Priti Desai <pdesai@us.ibm.com>
2016-12-23 12:23:02 -0800

    Create HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+    /* This is my second scala program
+       This will print "Hello World" as the output
+    */
+
+    def main (args: Array[String]) {
+        println ("Hello, World!") //print Hello World
+    }
+}
